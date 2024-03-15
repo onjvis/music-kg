c7ae8c0f8237e8218c987d6a86dc7895827b9e99
Ondřej Viskup <viskup@pm.me>
2024-03-15 20:11:13 +0100

    feat(dashboard): Warning alert

---
+++ apps/dashboard/src/components/alert/warning-alert.tsx
@@ -0,0 +1,21 @@
+import { IconContext } from 'react-icons';
+import { IoWarning } from 'react-icons/io5';
+
+import { Alert } from './alert';
+import { SharedAlertProps } from './models/shared-alert-props.model';
+
+export const WarningAlert = ({ message }: SharedAlertProps) => {
+  return (
+    <Alert
+      className="alert--warning"
+      icon={
+        <IconContext.Provider value={{ className: 'fill-yellow-400', size: '1.5em' }}>
+          <IoWarning />
+        </IconContext.Provider>
+      }
+      message={message}
+    />
+  );
+};
+
+export default WarningAlert;
