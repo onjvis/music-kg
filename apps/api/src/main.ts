61cd9329792fc7068a61535cb186a4265d23457a
Ondřej Viskup <viskup@pm.me>
2024-04-24 22:00:49 +0200

    feat(api): Expose Location header for incoming requests through CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,4 @@
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 
@@ -12,7 +12,9 @@ const port = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+
+const corsOptions: CorsOptions = { exposedHeaders: 'Location' };
+app.use(cors(corsOptions));
 
 connectToDatabase()
   .then(() => {
